191587ff37dd2fc1dc8a8920f5ca8b1835c39ea7
xdobrock <xdobrock@fi.muni.cz>
2026-02-07 00:30:35 +0100

    BREAKING CHANGE: create java spring boot project

---
+++ chessengine/src/main/java/com/github/chessengine/ChessengineApplication.java
@@ -0,0 +1,12 @@
+package com.github.chessengine;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessengineApplication {
+
+    public static void main(String[] args) {
+        System.out.print("lol");
+    }
+
+}
